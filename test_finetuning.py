0d1735dbc7e9a31d5a831aa890a4d0a81130efdb
Francesco Di Gangi <38263390+FDG2801@users.noreply.github.com>
2024-09-17 15:14:24 +0200

    update test

--- test_finetuning.py
+++ test_finetuning.py
@@ -76,9 +76,9 @@ if __name__ == '__main__':
     # Arguments parsing
     parser = argparse.ArgumentParser(description='Cross-Domain Few-Shot Semantic Segmentation Pytorch Implementation')
     #parser.add_argument('--datapath', type=str, default='../Dataset')
-    parser.add_argument('--datapath', type=str, default='./dataset')
+    parser.add_argument('--datapath', type=str, default='Dataset')
     parser.add_argument('--benchmark', type=str, default='isic', choices=['fss', 'deepglobe', 'isic', 'lung', 'pascal'])
-    parser.add_argument('--logpath', type=str, default='./test_logs')
+    parser.add_argument('--logpath', type=str, default='test_logs')
     parser.add_argument('--bsz', type=int, default=30)
     parser.add_argument('--nworker', type=int, default=0)
     parser.add_argument('--load', type=str, default='path_to_your_trained_model')
