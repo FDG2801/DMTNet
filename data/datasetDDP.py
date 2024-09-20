339cc0ffb1f0667553bf48bf0d8e271a29435f64
Francesco Di Gangi <38263390+FDG2801@users.noreply.github.com>
2024-09-20 11:49:15 +0200

    Update datasetDDP.py

--- data/datasetDDP.py
+++ data/datasetDDP.py
@@ -33,11 +33,11 @@ class FSSDataset:
         #New data augmentation for tests
         cls.transform = transforms.Compose([
                             transforms.Resize(size=(512, 512)),  # Nota: in torchvision, Resize accetta una tupla (height, width)
+                            transforms.ToTensor(),
                             transforms.Normalize(
                                 mean=[0.485, 0.456, 0.406],
                                 std=[0.229, 0.224, 0.225]
-                            ),
-                            transforms.ToTensor() 
+                            )
                     ])
 
     @classmethod
